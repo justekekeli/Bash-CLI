1f5748df894ace5c00b88a201c729d3296250d88
Kekeli Afonouvi <51093500+justekekeli@users.noreply.github.com>
2023-05-13 07:30:34 +0000

    adding an example of the generator

--- main.sh
+++ main.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+
+## reverse example
+cd reverse
 ./palindromes.sh | xargs ./reverse.sh # pass the palindromeS.sh output to reverse.sh as args with xargs
+
+## generator example
+cd ../generate
+./cli.sh --count 4 --phrase "Give the best you can"
